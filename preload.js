d7344cd3023c2f1ff2422c6f1039c325dd270331
Bobo100 <w150lione@gmail.com>
2024-08-07 18:09:08 +0800

    [what] 移除多餘的檔案

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electron", {
-  invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args),
-  on: (channel, callback) =>
-    ipcRenderer.on(channel, (event, ...args) => callback(...args)),
-  send: (channel, ...args) => ipcRenderer.send(channel, ...args),
-});
