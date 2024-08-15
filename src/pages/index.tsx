452396e92a584735262418997b44e0bb3ac03df7
Bobo100 <w150lione@gmail.com>
2024-08-15 18:28:55 +0800

    [what] 更新url title(用i18n)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { LinkListDetail } from "@/components/common/linkList";
 import HomeComponent from "@/components/home/homeComponent";
 import Layout from "@/components/layout";
+import { useTranslation } from "react-i18next";
 
 function HomePage() {
+  const { t } = useTranslation();
 
   return (
-    <Layout title={LinkListDetail["/"].title} content={LinkListDetail["/"].description}>
+    <Layout title={t(LinkListDetail["/"].title)} content={t(LinkListDetail["/"].description)}>
       <HomeComponent />
     </Layout>
   )
