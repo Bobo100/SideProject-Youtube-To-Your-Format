471a9e92298afd5ca9d8a27bb70d3cd5dab8e96f
Bobo100 <w150lione@gmail.com>
2024-08-17 20:54:55 +0800

    [what] 修改開啟伺服器的方法 改用electron-serve

--- main/modules/createWindow.mjs
+++ main/modules/createWindow.mjs
@@ -2,11 +2,14 @@ import { BrowserWindow } from "electron";
 import serve from "electron-serve";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
+// import { spawn } from "child_process";
+// import log from "electron-log";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// const loadURL = serve({ directory: "out" });
+const appServe = serve({ directory: "out" });
+// const appServe  = serve({ directory: path.join(__dirname, "..", "..", "out") });
 
 const createWindow = async (app) => {
   let win = new BrowserWindow({
@@ -19,8 +22,25 @@ const createWindow = async (app) => {
 
   if (app.isPackaged) {
     // await loadURL(win);
-    win.loadURL("http://localhost:3000"); // => 改用npx next start啟動next.js伺服器了 所以有網址
+    // win.loadURL("http://localhost:3000"); // => 改用npx next start啟動next.js伺服器了 所以有網址
     // win.webContents.openDevTools(); // 在打包环境中打开开发者工具
+
+    // const { spawn } = await import("child_process");
+    // const server = spawn("npx", ["next", "start"], {
+    //   cwd: path.join(__dirname, "..", ".."), // 项目根目录
+    //   shell: true,
+    // });
+    // log.info(`Next.js: Starting server in ${path.join(__dirname, "..", "..")}`);
+    // server.stdout.on("data", (data) => {
+    //   log.info(`Next.js: ${data}`);
+    // });
+    // server.stderr.on("data", (data) => {
+    //   log.error(`Next.js Error: ${data}`);
+    // });
+
+    appServe(win).then(() => {
+      win.loadURL("app://-");
+    });
   } else {
     win.loadURL("http://localhost:3000");
     win.webContents.openDevTools();
--- next.config.mjs
+++ next.config.mjs
@@ -6,16 +6,18 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: "out",
+  output: "export",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
   images: {
     unoptimized: true,
   },
-  i18n: {
-    locales: ["en", "zh"],
-    defaultLocale: "zh",
-  },
+  // 有output: "export"时，不能配置i18n
+  // i18n: {
+  //   locales: ["en", "zh"],
+  //   defaultLocale: "zh",
+  // },
   trailingSlash: true,
 };
 
